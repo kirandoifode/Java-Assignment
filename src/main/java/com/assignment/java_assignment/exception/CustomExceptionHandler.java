11b404ff1d8b8670f89885ff2ec7a51477ebce80
kirandoifode <kirandoifode5@gmail.com>
2021-07-24 21:33:26 +0530

    Msg changes

--- src/main/java/com/assignment/java_assignment/exception/CustomExceptionHandler.java
+++ src/main/java/com/assignment/java_assignment/exception/CustomExceptionHandler.java
@@ -3,12 +3,13 @@ package com.assignment.java_assignment.exception;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.assignment.java_assignment.controller.AdminController;
@@ -21,22 +22,24 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 	private String NOT_FOUND = "NOT_FOUND";
 
 	@ExceptionHandler(EmptyInputFieldException.class)
-	public final ResponseEntity<ErrorResponse> handleInvalidTraceIdException(EmptyInputFieldException ex,
-			WebRequest request) {
+	public final ResponseEntity<ErrorResponse> handleEmptyInputFieldException(EmptyInputFieldException ex,
+			HttpServletRequest request) {
 		List<String> details = new ArrayList<>();
 		details.add(ex.getLocalizedMessage());
+		details.add(request.getRequestURI());
 		ErrorResponse error = new ErrorResponse(BAD_REQUEST, details);
-		LOGGER.error("Error occurred during request processing. ", error);
+		LOGGER.error("Error occurred during request processing. "+details);
 		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(RecordNotFoundException.class)
 	public final ResponseEntity<ErrorResponse> handleUserNotFoundException(RecordNotFoundException ex,
-			WebRequest request) {
+			HttpServletRequest request) {
 		List<String> details = new ArrayList<>();
 		details.add(ex.getLocalizedMessage());
+		details.add(request.getRequestURI());
 		ErrorResponse error = new ErrorResponse(NOT_FOUND, details);
-		LOGGER.error("Error occurred during request processing. ", error);
+		LOGGER.error("Error occurred during request processing. "+details);
 		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
 	}
 
